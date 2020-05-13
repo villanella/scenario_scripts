e3dd4a71d5e2a5d6d4eee6852b54a8cefaf74d99
Nadezhda Krupenkina <laqwerta@gmail.com>
2020-05-13 18:04:14 +0300

    Dict with indents

--- yamlDictBuilder.py
+++ yamlDictBuilder.py
@@ -9,11 +9,12 @@ files = [item for item in os.listdir('.') if os.path.isfile(item)]
 current_file = os.path.basename(__file__)
 input_yaml_files = []
 for item in files:
-    if re.search(r"(.*(viber|Dict).yaml)", item):
+    if re.search(r"(.*(viber|Dict|Dictall).yaml)", item):
         continue
     if re.search(r"(.*qa.*.yaml)", item):
         input_yaml_files.append(item)
 print(input_yaml_files)
+yamldicts = []
 
 
 
@@ -30,17 +31,22 @@ check = 'false'
 q_list = 'false'
 added_parent = [] 
 
-#Dict = { }
+Dict = { }
+dataSet = [] 
+DictSet = { }
 #print("Initial nested dictionary") 
 
 #with open(output_file_name, 'w', encoding='utf-8') as result_file:
 for file in input_yaml_files:
     print(file)
-    Dict = { }
+    #Dict = { }
     regspace = ''
     expDictName = file[0:-5] + 'Dict.yaml'
     with open(file, 'r', encoding='utf-8') as input_file:
         for line in input_file:
+            if line.lstrip().startswith('#'):
+                #print(line)
+                continue
             state_search = re.search(yaml_state, line)
             if state_search:
                 if len(state_search.group(2)) <= (len(regspace) + 5):
@@ -50,13 +56,14 @@ for file in input_yaml_files:
                     #print("regstate = " + regstate)
                     #print("regspace = '" + regspace + "'")
                     Dict[regspace + regstate] = {}
-                    #Dict[regspace + regstate]["indent"] = regspace
+                    Dict[regspace + regstate]["indent"] = regspace
                     check = 'false'
                     q_list = 'false'
             q_search = re.search(yaml_q, line)
             if q_search:
                 q_list = 'true'
                 Dict[regspace + regstate][regspace + 'q'] = []
+                DictSet[regstate] = []
                 #print("q: = " + line)
             a_search = re.search(yaml_a, line)
             if a_search:
@@ -66,6 +73,7 @@ for file in input_yaml_files:
                 regexample = example_search.group(2)[:-1]
                 #print("regexample " + regexample)
                 Dict[regspace + regstate][regspace + 'q'].append(regspace + regexample)
+                DictSet[regstate].append(regexample)
             else:
                 if check == 'true':
                     #странно обрабатываются некоторые символы, поэтому такой костыль
@@ -90,7 +98,7 @@ for file in input_yaml_files:
                     else:
                         line = re.sub(line, '    ' + line, line)
 #            result_file.write(line)
-        print(Dict)
+        #print(DictSet)
         print("словарь готов")
             
 
@@ -101,6 +109,36 @@ for file in input_yaml_files:
         yaml.default_flow_style = False 
         with open(expDictName, 'w', encoding='utf-8') as f:
             data = yaml.dump(Dict, f)
-            
+            yamldicts.append(expDictName)
+
+print(len(DictSet.items()))
+#Example Statistics            
+samplesToAdd = {}
+zeroStates = []
+for key, value in DictSet.items():
+    samSum = len(DictSet[key])
+    #print(key, samSum)
+    if (samSum > 0) and (samSum < 20):
+        samplesToAdd[key] = 20-samSum
+    if samSum == 0:
+        zeroStates.append(key) 
+print(len(zeroStates))
+print(len(samplesToAdd))
+
+
+#Write classifier_samples.yaml
+with open('classifier_samples.yaml', 'w', encoding='utf-8') as classifier_samples:
+    for file in yamldicts:
+        print(file)
+        with open(file, 'r', encoding='utf-8') as input_yaml:
+            for line in input_yaml:
+                #re.sub(r'(^[']|[']$|':|{}), '', line)
+                line = re.sub(r"[,@\'?\$%_#{}\[\]]", "", line)
+                if 'q:' in line:
+                    line = re.sub(r"([\s#]*)q:", r"\1qs:\n\1  q:", line)
+                if re.findall(r"^\s*-([\s]*)(.*)", line):
+                    line = re.sub(r"-([\s]*)(.*)", r"\1- \2", line)
+                #print(line)
+                classifier_samples.write(line)
 
             
